13565301b6e20841cfb96d865cf9736475feb2e6
Lado Samushia <lado.samushia.office@gmail.com>
2021-06-16 16:20:33 +0400

    added preliminary gridding routines

--- test.jl
+++ test.jl
@@ -1,3 +1,7 @@
+using FITSIO
+using DataFrames
+using QuadGK
+
 """
     Create index of nonzero cells.
 """
@@ -40,4 +44,48 @@ function pair_loop(index, pairs)
         end
     end
     return pairs
+end
+
+"""
+    Read eBOSS catalog from fits fits
+"""
+function read_fits_catalog(fitsfile)
+    f = FITS(fitsfile)
+    df = DataFrame(f[2])
+    return df
+end
+
+function spherical_to_cartesian(ra, dec, z)
+    ϕ = ra*π/180
+    θ = dec*π/180 .+ π/2
+    distance(z) = 2999/sqrt(0.3*(1+z)^3+0.7)
+    r = zeros(length(z))
+    for i in 1:length(r)
+        r[i] = quadgk(distance, 0, z[i])[1]
+    end
+    x = r.*cos.(ϕ).*sin.(θ)
+    y = r.*sin.(ϕ).*sin.(θ)
+    z = r.*cos.(θ)
+    return x, y, z
+end
+
+"""
+    grid eBOSS data.
+"""
+function grid_survey(x, y, z, Ngrid)
+    grid = zeros(Ngrid, Ngrid, Ngrid)
+    xmin = minimum(x)
+    ymin = minimum(y)
+    zmin = minimum(z)
+    Lx = maximum(x) - xmin
+    Ly = maximum(y) - ymin
+    Lz = maximum(z) - zmin
+    L = maximum([Lx, Ly, Lz])
+    for i in 1:length(x)
+        ix = ceil(Int, (x[i] - xmin + 0.0001)/(L + 0.0001)*Ngrid)
+        iy = ceil(Int, (y[i] - ymin + 0.0001)/(L + 0.0001)*Ngrid)
+        iz = ceil(Int, (z[i] - zmin + 0.0001)/(L + 0.0001)*Ngrid)
+        grid[ix, iy, iz] += 1
+    end
+    return grid, L/Ngrid
 end
